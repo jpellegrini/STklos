de1c031f00351d03ad068e97cdc7c508ac84fac6
Jeronimo Pellegrini <j_p@aleph0.info>
2026-09-03 21:14:36 -0300

    Don't include inexact zero in (* 2.0 +3i)
    when we do (* +3i 2.0) we should get +6.0i.  However, if we fall
    through and use mul2(r1,o2) we will have real part "0 times 2.0" which
    does NOT result in 0, but in 0.0 (inexactness contaminates).  But in
    this case it makes no sense. Schemes do vary with respect to this, but
    since STklos uses mixed exactness in complexes, it makes sense to
    return +6.0i. So we test for real part == 0, and when it is, we
    just use plain MAKE_INT(0) when calling make_compelx.

--- src/number.c
+++ src/number.c
@@ -2284,7 +2284,17 @@ SCM STk_mul2(SCM o1, SCM o2)
                             return make_complex(sub2(mul2(r1,r2), mul2(i1, i2)),
                                               add2(mul2(r1,i2), mul2(r2, i1)));
                           }
-        case tc_real:     // fallthrough
+            /* Below: when we do (* +3i 2.0)  we should get +6.0i.
+                      However, if we fall through and use mul2(r1,o2)
+                      we will have real part "0 times 2.0" which does
+                      NOT result in 0, but in 0.0 (inexactness contaminates).
+                      But in this case it makes no sense. Schemes do vary
+                      with respect to this, but since STklos uses mixed
+                      exactness in complexes, it makes sense to return
+                      +6.0i. So we test for r1 == 0. */
+        case tc_real:     if (r1 == MAKE_INT(0))
+                            return make_complex(MAKE_INT(0), mul2(i1, o2));
+                          // fallthrough
         case tc_rational: // fallthrough
         case tc_bignum:   // fallthrough
         case tc_integer:  return make_complex(mul2(r1, o2), mul2(i1, o2));
@@ -2394,8 +2404,19 @@ SCM STk_mul2(SCM o1, SCM o2)
   error_cannot_operate("multiplication", o1, o2);
 
  mult_x_and_complex:    // x here can be a real, a rational, a bignum or a fixnum
+  /* Below: when we do (* 2.0 +3i)  we should get +6.0i.
+            However, if we fall through and use mul2(r1,o2)
+            we will have real part "0 times 2.0" which does
+            NOT result in 0, but in 0.0 (inexactness contaminates).
+            But in this case it makes no sense. Schemes do vary
+            with respect to this, but since STklos uses mixed
+            exactness in complexes, it makes sense to return
+            +6.0i. So we test for real part == 0. */
+  if (COMPLEX_REAL(o2) == MAKE_INT(0))
+      return make_complex(MAKE_INT(0),
+                          mul2(o1,COMPLEX_IMAG(o2)));
   return make_complex(mul2(o1,COMPLEX_REAL(o2)),
-                       mul2(o1,COMPLEX_IMAG(o2)));
+                      mul2(o1,COMPLEX_IMAG(o2)));
 }
 
 DEFINE_PRIMITIVE("*", multiplication, vsubr, (int argc, SCM *argv))
